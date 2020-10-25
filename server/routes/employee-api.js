2752fd089f56833e5507ce9b5c9485c12a3e5b48
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-10-25 15:14:36 -0500

    refactored to remove duplicate code

--- server/routes/employee-api.js
+++ server/routes/employee-api.js
@@ -19,111 +19,80 @@ const ErrorResponse = require('../services/error-response');
 let router = express.Router();
 
 /******************************************************************************
- * FindOne function awaits results from the Mongo model.findOne function.
- * The results are attached to the response object, which is magically
- * passed to the next function in the callback chain.
+ * Find One
  *****************************************************************************/
-let findOne = async (req, res) => {
-  try {
-    await Employee.findOne({
-      'id': req.params.id
-    }, res.locals.selection, function (error, employee) {
+let findOne = async (req, selection) => {
 
-      if (error) {
-        res.locals.result = {
-          error: error
-        };
-        console.log(error);
-      } else {
-        console.log(employee);
-        res.locals.result = {
-          success: employee
-        };
-      }
-    });
+  let employee;
+
+  try {
+    const id = req.params.id;
+    employee = await Employee.findOne({'id': id}, selection).exec();
+    console.log(employee);
   } catch (e) {
     console.log(e);
-    res.status(500).send(new ErrorResponse(e.message).toObject());
+    return {error: e};
   }
-}
+  return {success: employee};
+};
 
 /******************************************************************************
- * Refactored Find employee by ID API
+ * Send Result
  *****************************************************************************/
-router.get('/:id', async (req, res, next) => {
-    res.locals.selection = '';
-    await findOne(req, res, next);
-    next();
-  },
-  // TODO: extract the following anonymous function to "sendResult()" and share
-  function (req, res) {
-
-    if (res.locals.result.error) {
-      const errorResponse = new ErrorResponse(res.locals.result.error);
-      res.status(500).send(errorResponse.toObject());
-    } else if (res.locals.result.success) {
-      const successResponse = new BaseResponse(res.locals.result.success);
-      res.json(successResponse.toObject());
-    }
-  })
+let sendResult = async (result, res) => {
+  if (result.error) {
+    const errorResponse = new ErrorResponse(result.error);
+    res.status(500).send(errorResponse.toObject());
+  } else if (result.success) {
+    const successResponse = new BaseResponse(result.success);
+    res.json(successResponse.toObject());
+  }
+};
 
 /******************************************************************************
- * Refactored Find all tasks by ID API
+ * Save doc
  *****************************************************************************/
-router.get('/:id/tasks', async (req, res, next) => {
-    res.locals.selection = 'id todo done';
-    await findOne(req, res, next);
-    next();
-  },
-  function (req, res) {
-    if (res.locals.result.error) {
-      const errorResponse = new ErrorResponse(res.locals.result.error);
-      res.status(500).send(errorResponse.toObject());
-    } else if (res.locals.result.success) {
-      const successResponse = new BaseResponse(res.locals.result.success);
-      res.json(successResponse.toObject());
-    }
-  })
+let saveDoc = async (result, res) => {
+  let employee = result.success;
+  try {
+    result.success = await employee.save();
+    await sendResult(result, res);
+  } catch (e) {
+    await sendResult(e, res);
+  }
+};
+
+/******************************************************************************
+ * Find employee by ID API
+ *****************************************************************************/
+router.get('/:id', async (req, res) => {
+  const selection = '';
+  let result = await findOne(req, selection);
+  await sendResult(result, res);
+})
+
+/******************************************************************************
+ * Find all tasks by ID API
+ *****************************************************************************/
+router.get('/:id/tasks', async (req, res) => {
+  const selection = 'id todo done';
+  let result = await findOne(req, selection);
+  await sendResult(result, res);
+})
 
 /******************************************************************************
  * Create task by ID API
  *****************************************************************************/
 router.post('/:id/tasks', async (req, res) => {
+  const selection = '';
 
-  try {
-    Employee.findOne({
-      "id": req.params.id
-    }, function (error, employee) {
-      if (error) {
-        console.log(error);
-        const errorResponse = new ErrorResponse(error);
-        res.status(500).send(errorResponse.toObject());
-      } else {
-        const item = {
-          text: req.body.text
-        };
+  let result = await findOne(req, selection);
+  const item = {text: req.body.text};
 
-        employee.todo.push(item);
+  let employee = result.success;
+  employee.todo.push(item);
 
-        employee.save(function (err, updatedEmployee) {
-          if (err) {
-            console.log(err);
-            const createTaskOnSaveMongoDbErrorResponse = new ErrorResponse(err);
-            res.status(500).send(createTaskOnSaveMongoDbErrorResponse.toObject());
-          } else {
-            console.log(updatedEmployee);
-            const createTaskOnSaveSuccessResponse = new BaseResponse(updatedEmployee);
-            res.json(createTaskOnSaveSuccessResponse.toObject());
-          }
-        })
-      }
-    })
-    // catch any other errors
-  } catch (e) {
-    console.log(e);
-    const catchErrorResponse = new ErrorResponse(e.message);
-    res.status(500).send(catchErrorResponse.toObject());
-  }
+  await saveDoc(result, res);
 })
 
 /******************************************************************************
@@ -132,42 +101,17 @@ router.post('/:id/tasks', async (req, res) => {
  * the request body.
  *****************************************************************************/
 router.put('/:id/tasks', async (req, res) => {
+  const selection = '';
 
-  try {
-    Employee.findOne({
-      "id": req.params.id
-    }, function (error, employee) {
+  let result = await findOne(req, selection);
+  let employee = result.success;
 
-      if (error) {
-        console.log(error);
-        const errorResponse = new ErrorResponse(error);
-        res.status(500).send(errorResponse.toObject());
-      } else {
+  employee.set({
+    todo: req.body.todo,
+    done: req.body.done
+  })
 
-        employee.set({
-          todo: req.body.todo,
-          done: req.body.done
-        })
-
-        employee.save(function (err, updatedEmployee) {
-          if (err) {
-            console.log(err);
-            const updateTaskOnSaveMongoDbErrorResponse = new ErrorResponse(err);
-            res.status(500).send(updateTaskOnSaveMongoDbErrorResponse.toObject());
-          } else {
-            console.log(updatedEmployee);
-            const updateTaskOnSaveSuccessResponse = new BaseResponse(updatedEmployee);
-            res.json(updateTaskOnSaveSuccessResponse.toObject());
-          }
-        })
-      }
-    });
-    // catch errors
-  } catch (e) {
-    console.log(e);
-    const catchErrorResponse = new ErrorResponse(e.message);
-    res.status(500).send(catchErrorResponse.toObject());
-  }
+  await saveDoc(result, res);
 })
 
 /******************************************************************************
@@ -176,65 +120,30 @@ router.put('/:id/tasks', async (req, res) => {
  * to eliminate the need to check both arrays by checking the source element.
  *****************************************************************************/
 router.delete('/:id/tasks/:taskId', async (req, res) => {
+  const selection = '';
 
-  try {
-    Employee.findOne({
-      "id": req.params.id
-    }, function (error, employee) {
+  let result = await findOne(req, selection);
+  let employee = result.success;
 
-      if (error) {
-        console.log(error);
-        const errorResponse = new ErrorResponse(error);
-        res.status(500).send(errorResponse.toObject());
+  const todoItem = employee.todo.find(item =>
+    item._id.toString() === req.params.taskId);
 
-      } else {
-        console.log(employee);
+  const doneItem = employee.done.find(item =>
+    item._id.toString() === req.params.taskId);
 
-        const todoItem = employee.todo.find(item => item._id.toString() === req.params.taskId);
-        const doneItem = employee.done.find(item => item._id.toString() === req.params.taskId);
+  if (todoItem) {
+    employee.todo.id(todoItem._id).remove();
+    await saveDoc(result, res);
 
-        if (todoItem) {
-          employee.todo.id(todoItem._id).remove();
-          employee.save(function (err, updatedTodoItemEmployee) {
-            if (err) {
-              console.log(err);
-              const deleteToDoItemOnSaveMongoDbErrorResponse = new ErrorResponse(err);
-              res.status(500).send(deleteToDoItemOnSaveMongoDbErrorResponse.toObject());
+  } else if (doneItem) {
+    employee.done.id(doneItem._id).remove();
+    await saveDoc(result, res);
 
-            } else {
-              console.log(updatedTodoItemEmployee);
-              const deleteToDoItemSuccessResponse = new BaseResponse(updatedTodoItemEmployee);
-              res.json(deleteToDoItemSuccessResponse.toObject());
-
-            }
-          })
-        } else if (doneItem) {
-          employee.done.id(doneItem._id).remove();
-
-          employee.save(function (err, updatedDoneItemEmployee) {
-            if (err) {
-              console.log(err);
-              const deleteDoneItemOnSaveMongoDbErrorResponse = new ErrorResponse(err);
-              res.status(500).send(deleteDoneItemOnSaveMongoDbErrorResponse.toObject());
-            } else {
-              console.log(updatedDoneItemEmployee);
-              const deleteDoneItemSuccessResponse = new BaseResponse(updatedDoneItemEmployee);
-              res.json(deleteDoneItemSuccessResponse.toObject());
-
-            }
-          })
-        } else {
-          console.log('Invalid task id');
-          const deleteTaskNotFoundResponse = new ErrorResponse(null, '200', "Unable to locate requested task");
-          res.status(200).send(deleteTaskNotFoundResponse.toObject());
-        }
-      }
-    });
-    // catch any other errors
-  } catch (e) {
-    console.log(e);
-    const catchErrorResponse = new ErrorResponse(e.message);
-    res.status(500).send(catchErrorResponse.toObject());
+  } else {
+    console.log('Invalid task id');
+    const errorResponse = new ErrorResponse(null, '200',
+      "Unable to locate requested task");
+    res.status(200).send(errorResponse.toObject());
   }
 })
 
